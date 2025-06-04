698f07f6cc92f2c80824c102a80541a9ead60cd9
kmj123 <kkk990108@naver.com>
2025-06-04 18:30:33 +0900

    0604디장고

---
+++ w0604/w02/board/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from board.models import Board
+
+# 게시글 쓰기
+def write(request):
+    if request.method =="GET":
+        return render(request,'board/write.html')
+    elif request.mehtod == "POST":
+        # request로 데이터 가져오기
+        id = request.POST.get('id')
+        btitle = request.POST.get('btitle')
+        bcontent = request.POST.get('bcontent')
+        bfile = request.POST.get('bfile')
+        
+        # 받아온 데이터 저장
+        qs = Board.objects.create(id=id,btitle=btitle,bcontent=bcontent,bfile=bfile)
+        qs.bgroup = qs.bno
+        qs.save()
+        context = {'msg': 1}
+        return render(request,'board/write.html',context)
+
+# 게시글 상세보기
+def view(request,bno):
+    qs = Board.objects.get(bno=bno)
+    context = {'board':qs}
+    return render(request,'board/view.html',context)
+
+# 게시글 목록
+def list(request):
+    qs = Board.objects.all()
+    context = {'list':qs}
+    return render(request,'board/list.html',context)
